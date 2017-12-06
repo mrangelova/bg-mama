b2c946c31cae8f3b76e9c13160d615af97afffe1
Emil Nik <emil.nik1002@gmail.com>
2017-12-06 03:26:54 +0200

    updated crawler

--- src/extractor/crawler.py
+++ src/extractor/crawler.py
@@ -1,6 +1,7 @@
 import database
 from helpers import make_request
 from amazon_model import AmazonModel
+from review_model import ReviewModel
 
 
 def parse_model(page):
@@ -9,26 +10,40 @@ def parse_model(page):
     if bs_product_name:
         raw_product_name = bs_product_name.text
     product_name = ''.join(raw_product_name).strip()
-    reviews = page.findAll('div', attrs={'data-hook': 'review'})
+    print('Getting reviews for {}.'.format(product_name))
+
     reviews_list = []
+    all_reviews_url = page.find(
+       'a', attrs={'data-hook': 'see-all-reviews-link-foot'})['href']
 
-    for review in reviews:
-        raw_review_rating = review.find(
-            'i', attrs={'data-hook': 'review-star-rating'}).text
-        review_text = review.find(
-            'div', attrs={'data-hook': 'review-collapsed'}).text
-        review_header = review.find(
-            'a', attrs={'data-hook': 'review-title'}).text
+    reviews_page = make_request(all_reviews_url)
+    next_reviews_page_button = reviews_page.find(
+        'li', attrs={'class': 'a-last'})
+    while next_reviews_page_button:
+        # TODO: Thread.sleep() ??????????????????????????????????????????
+        reviews = reviews_page.findAll(
+            'div', attrs={'data-hook': 'review'})
+        for review in reviews:
+            raw_review_rating = review.find(
+                'i', attrs={'data-hook': 'review-star-rating'}).text
+            review_text = review.find(
+                'span', attrs={'data-hook': 'review-body'}).text
+            review_header = review.find(
+                'a', attrs={'data-hook': 'review-title'}).text
 
-        review_rating = ''.join(raw_review_rating).replace(
-            ' out of 5 stars', '')
+            review_rating = ''.join(raw_review_rating).replace(
+                ' out of 5 stars', '')
 
-        review_dict = {
-            'review_header': review_header,
-            'review_text': review_text,
-            'review_rating': review_rating
-        }
-        reviews_list.append(review_dict)
+            review_model = ReviewModel(
+                review_header, review_text, review_rating)
+            reviews_list.append(review_model)
+        next_reviews_page_button = reviews_page.find(
+            'li', attrs={'class': 'a-last'})
+        if 'a-disabled' not in next_reviews_page_button['class']:
+            next_url = next_reviews_page_button.find('a')['href']
+            reviews_page = make_request(next_url)
+        else:
+            next_reviews_page_button = None
 
     model = AmazonModel(product_name, reviews_list)
     return model
@@ -46,8 +61,8 @@ def start_crawl():
         data = parse_model(page)
         result.append(data)
     database.save(result)
+    print("{} reviews found.".format(len(data.reviews)))
 
 
 if __name__ == "__main__":
     start_crawl()
-    print("End")
--- src/extractor/settings.py
+++ src/extractor/settings.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 # database
-db_connection_string = "data/db-{}.txt".format(datetime.now().strftime("%d-%m-%Y %H-%M-%S"))
+db_connection_string = "data/db.txt"  # .format(datetime.now().strftime("%d-%m-%Y %H-%M-%S"))
 
 # start file
 start_file = "extractor/start_urls.txt"
 
-allowed_params = ["node", "rh", "page"]
+allowed_params = ["node", "rh", "page", "pageNumber"]
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     "Accept-Encoding": "gzip, deflate, sdch, br",
