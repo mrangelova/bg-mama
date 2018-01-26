0b2e8a5cceedcd330fbbd9135b93dc7a2bc8a424
Emil Nik <emil.nik1002@gmail.com>
2018-01-26 21:08:00 +0200

    stop words. to remove or not to remove :?

--- src/preprocessing/review_tokenizer.py
+++ src/preprocessing/review_tokenizer.py
@@ -4,10 +4,11 @@ from nltk.corpus import stopwords
 
 class ReviewTokenizerMixin:
     def tokenize(self):
-        words = TweetTokenizer(preserve_case=False).tokenize(self.text)
-        newWords = []
-        stopWords = set(stopwords.words('english'))
-        for word in words:
-            if word not in stopWords:
-                newWords.append(word)
-        return newWords
+        # words = TweetTokenizer(preserve_case=False).tokenize(self.text)
+        # newWords = []
+        # stopWords = set(stopwords.words('english'))
+        # for word in words:
+        #     if word not in stopWords:
+        #         newWords.append(word)
+        # return newWords
+        return TweetTokenizer(preserve_case=False).tokenize(self.text)
