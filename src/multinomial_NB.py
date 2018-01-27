eb30b9ae16769ca5e39cbf07c9ceeb2cb66c9ea3
Emil Nik <emil.nik1002@gmail.com>
2018-01-27 13:11:18 +0200

    naive fixnaive fix

--- src/multinomial_NB.py
+++ src/multinomial_NB.py
@@ -3,10 +3,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from reviews import AmazonReviews, BGMammaReviews
+import numpy as np
 
 
-def get_sparse_matrix(reviews):
-    bow_transformer = CountVectorizer().fit(reviews)
+def get_sparse_matrix(reviews, reviews_test=[]):
+    bow_transformer = CountVectorizer().fit(reviews, reviews_test)
     print(len(bow_transformer.vocabulary_))
     reviews = bow_transformer.transform(reviews)
     return reviews
@@ -20,11 +21,8 @@ def split_reviews_to_text_and_rating(reviews):
 
 def get_accuracy(nb, rev_text_test, rev_ratings_test):
     predictions = nb.predict(rev_text_test)
-
-    print(confusion_matrix(rev_ratings_test, predictions))
-    print('\n')
-    print(classification_report(rev_ratings_test, predictions))
     print('Accuracy: ', accuracy_score(rev_ratings_test, predictions))
+    print(classification_report(rev_ratings_test, predictions))
 
 
 def multinominal_NB(reviews):
@@ -51,15 +49,15 @@ def multinominal_NB_test(reviews, test_reviews):
     rev_texts, rev_ratings = split_reviews_to_text_and_rating(reviews)
     rev_texts_test, rev_ratings_test = split_reviews_to_text_and_rating(test_reviews)
 
-    rev_texts = get_sparse_matrix(rev_texts)
-    rev_texts_test = get_sparse_matrix(rev_texts_test)
+    rev_texts_matrix = get_sparse_matrix(rev_texts, rev_texts_test)
+    rev_texts_test_matrix = get_sparse_matrix(rev_texts_test, rev_texts)
     
     # train classifier
     nb = MultinomialNB()
-    nb.fit(rev_texts, rev_ratings)
+    nb.fit(rev_texts_matrix, rev_ratings)
     
     # test classifier
-    get_accuracy(nb, rev_texts_test, rev_ratings_test)
+    get_accuracy(nb, rev_texts_test_matrix, rev_ratings_test)
 
 
 def start():
@@ -71,12 +69,16 @@ def start():
     
     print('Training classifier with bg-mamma reviews.')
     reviews_bgmamma = BGMammaReviews.get()
+    reviews_bgmamma.remove_irrelevant_posts()
     reviews_bgmamma = BGMammaReviews(reviews_bgmamma.dropna())
-    reviews_bgmamma = reviews_bgmamma.tokenize()
+    # reviews_bgmamma = reviews_bgmamma.tokenize()
     multinominal_NB(reviews_bgmamma)
 
     print('Training classifier with all reviews.')
     reviews_all = reviews_amazon.append(reviews_bgmamma)
     multinominal_NB(reviews_all)
+
+    print('Classifying bg-mamma reviews with amazon train')
+    multinominal_NB_test(reviews_amazon, reviews_bgmamma)
 
 start()
