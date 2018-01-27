3c0f9aafe83b0bf1fa5d2da1084f045ca9cf64a4
Emil Nik <emil.nik1002@gmail.com>
2018-01-27 10:24:21 +0200

    naive fix

--- src/multinomial_NB.py
+++ src/multinomial_NB.py
@@ -1,9 +1,8 @@
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, classification_report
-from amazon_reviews import AmazonReviews
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from reviews import AmazonReviews, BGMammaReviews
 
 
 def get_sparse_matrix(reviews):
@@ -25,22 +24,15 @@ def get_accuracy(nb, rev_text_test, rev_ratings_test):
     print(confusion_matrix(rev_ratings_test, predictions))
     print('\n')
     print(classification_report(rev_ratings_test, predictions))
+    print('Accuracy: ', accuracy_score(rev_ratings_test, predictions))
 
 
-def check_if_nan(reviews):
-    for review in reviews['rating']:
-        if review is np.nan:
-            review = 0
-
-
-def multinominal_NB():
-    reviews = AmazonReviews.get()
-    reviews = reviews.round_ratings()
-    check_if_nan(reviews)
+def multinominal_NB(reviews):
+    reviews.round_ratings()
     rev_texts, rev_ratings = split_reviews_to_text_and_rating(reviews)
 
     rev_texts = get_sparse_matrix(rev_texts)
-    
+
     # get train and test data
     rev_text_train, rev_text_test, rev_ratings_train, rev_ratings_test = train_test_split(
         rev_texts, rev_ratings, test_size=0.2)
@@ -52,5 +44,39 @@ def multinominal_NB():
     # test classifier
     get_accuracy(nb, rev_text_test, rev_ratings_test)
 
+    
+def multinominal_NB_test(reviews, test_reviews):
+    reviews.round_ratings()
+    test_reviews.round_ratings()
+    rev_texts, rev_ratings = split_reviews_to_text_and_rating(reviews)
+    rev_texts_test, rev_ratings_test = split_reviews_to_text_and_rating(test_reviews)
 
-multinominal_NB()
+    rev_texts = get_sparse_matrix(rev_texts)
+    rev_texts_test = get_sparse_matrix(rev_texts_test)
+    
+    # train classifier
+    nb = MultinomialNB()
+    nb.fit(rev_texts, rev_ratings)
+    
+    # test classifier
+    get_accuracy(nb, rev_texts_test, rev_ratings_test)
+
+
+def start():
+    print('Training classifier with amazon reviews.')
+    reviews_amazon = AmazonReviews.get()
+    reviews_amazon = AmazonReviews(reviews_amazon.dropna())
+    reviews_amazon.tokenize()
+    multinominal_NB(reviews_amazon)
+    
+    print('Training classifier with bg-mamma reviews.')
+    reviews_bgmamma = BGMammaReviews.get()
+    reviews_bgmamma = BGMammaReviews(reviews_bgmamma.dropna())
+    reviews_bgmamma = reviews_bgmamma.tokenize()
+    multinominal_NB(reviews_bgmamma)
+
+    print('Training classifier with all reviews.')
+    reviews_all = reviews_amazon.append(reviews_bgmamma)
+    multinominal_NB(reviews_all)
+
+start()
